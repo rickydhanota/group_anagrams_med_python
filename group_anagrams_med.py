b913694559ee6284cef4360a9900832113475271
Ricky <rickdhanota@gmail.com>
2020-09-29 23:13:36 -0700

    initial commit

---
+++ group_anagrams_med.py
@@ -0,0 +1,8 @@
+# Write a function that takes in an array of strings and groups anagrams together. Anagrams are strings made up of exactly the same letters, where order doesnt matter. For example, "cinema" and "iceman" are anagrams, similarily, "foo" and "ofo" are anagrams
+# Your function should return a list of anagram groups in no parituclar order.
+
+# sample input; 
+#words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+
+# Sample output:
+# output = [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
